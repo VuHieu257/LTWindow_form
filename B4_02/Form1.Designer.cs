8f9033b000021aa0f1926b35a7e2c320a436103e
VuHieu257 <144141957+VuHieu257@users.noreply.github.com>
2023-10-03 23:21:26 +0700

    Add files via upload
    homework 4_2

--- B4_02/Form1.Designer.cs
+++ B4_02/Form1.Designer.cs
@@ -32,23 +32,33 @@
             "01",
             "Coffe đá",
             "15000"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "02",
+            "Cà phê sữa",
+            "18000"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+            "03",
+            "Nước cam",
+            "25000"}, -1);
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.listViewSV = new System.Windows.Forms.ListView();
-            this.lvHoTen = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.lvKhoa = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvDatMon = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.btnRemoveAll = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CbSoLuong = new System.Windows.Forms.ComboBox();
             this.btnRemove = new System.Windows.Forms.Button();
-            this.btnAddAll = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.cbbKhoa = new System.Windows.Forms.ComboBox();
+            this.cbbBan = new System.Windows.Forms.ComboBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.lvMon = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chMon = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox4.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -56,46 +66,60 @@
             // 
             // groupBox4
             // 
-            this.groupBox4.Controls.Add(this.listViewSV);
+            this.groupBox4.Controls.Add(this.lvDatMon);
             this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox4.Location = new System.Drawing.Point(692, 188);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(465, 360);
+            this.groupBox4.Size = new System.Drawing.Size(471, 360);
             this.groupBox4.TabIndex = 3;
             this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Sinh Viên Được Chọn";
+            this.groupBox4.Text = "Danh sách  đặt món";
             // 
-            // listViewSV
+            // lvDatMon
             // 
-            this.listViewSV.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.lvHoTen,
-            this.lvKhoa});
-            this.listViewSV.HideSelection = false;
-            this.listViewSV.Location = new System.Drawing.Point(13, 26);
-            this.listViewSV.Name = "listViewSV";
-            this.listViewSV.Size = new System.Drawing.Size(428, 328);
-            this.listViewSV.TabIndex = 0;
-            this.listViewSV.UseCompatibleStateImageBehavior = false;
-            this.listViewSV.View = System.Windows.Forms.View.Details;
+            this.lvDatMon.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lvDatMon.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvDatMon.GridLines = true;
+            this.lvDatMon.HideSelection = false;
+            this.lvDatMon.Location = new System.Drawing.Point(3, 22);
+            this.lvDatMon.Name = "lvDatMon";
+            this.lvDatMon.Size = new System.Drawing.Size(465, 335);
+            this.lvDatMon.TabIndex = 0;
+            this.lvDatMon.UseCompatibleStateImageBehavior = false;
+            this.lvDatMon.View = System.Windows.Forms.View.Details;
             // 
-            // lvHoTen
+            // columnHeader4
             // 
-            this.lvHoTen.Text = "Họ Tên";
-            this.lvHoTen.Width = 159;
+            this.columnHeader4.Text = "Bàn";
+            this.columnHeader4.Width = 66;
             // 
-            // lvKhoa
+            // columnHeader5
             // 
-            this.lvKhoa.Text = "Khoa";
-            this.lvKhoa.Width = 213;
+            this.columnHeader5.Text = "Tên món";
+            this.columnHeader5.Width = 130;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Số Lượng ";
+            this.columnHeader6.Width = 94;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Giá";
+            this.columnHeader7.Width = 58;
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.btnRemoveAll);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.CbSoLuong);
             this.groupBox3.Controls.Add(this.btnRemove);
-            this.groupBox3.Controls.Add(this.btnAddAll);
             this.groupBox3.Controls.Add(this.btnAdd);
             this.groupBox3.Controls.Add(this.label2);
-            this.groupBox3.Controls.Add(this.cbbKhoa);
+            this.groupBox3.Controls.Add(this.cbbBan);
             this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox3.Location = new System.Drawing.Point(450, 188);
             this.groupBox3.Name = "groupBox3";
@@ -104,73 +128,87 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Chức Năng";
             // 
-            // btnRemoveAll
+            // label3
             // 
-            this.btnRemoveAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRemoveAll.Location = new System.Drawing.Point(21, 298);
-            this.btnRemoveAll.Name = "btnRemoveAll";
-            this.btnRemoveAll.Size = new System.Drawing.Size(198, 47);
-            this.btnRemoveAll.TabIndex = 2;
-            this.btnRemoveAll.Text = "<<";
-            this.btnRemoveAll.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Số Lượng";
+            // 
+            // CbSoLuong
+            // 
+            this.CbSoLuong.FormattingEnabled = true;
+            this.CbSoLuong.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20"});
+            this.CbSoLuong.Location = new System.Drawing.Point(21, 124);
+            this.CbSoLuong.Name = "CbSoLuong";
+            this.CbSoLuong.Size = new System.Drawing.Size(198, 28);
+            this.CbSoLuong.TabIndex = 3;
             // 
             // btnRemove
             // 
             this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRemove.Location = new System.Drawing.Point(21, 236);
+            this.btnRemove.Location = new System.Drawing.Point(21, 274);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(198, 47);
             this.btnRemove.TabIndex = 2;
-            this.btnRemove.Text = "<";
+            this.btnRemove.Text = "Hủy";
             this.btnRemove.UseVisualStyleBackColor = true;
-            // 
-            // btnAddAll
-            // 
-            this.btnAddAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddAll.Location = new System.Drawing.Point(21, 167);
-            this.btnAddAll.Name = "btnAddAll";
-            this.btnAddAll.Size = new System.Drawing.Size(198, 47);
-            this.btnAddAll.TabIndex = 2;
-            this.btnAddAll.Text = ">>";
-            this.btnAddAll.UseVisualStyleBackColor = true;
             // 
             // btnAdd
             // 
             this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(21, 99);
+            this.btnAdd.Location = new System.Drawing.Point(21, 192);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(198, 47);
             this.btnAdd.TabIndex = 2;
-            this.btnAdd.Text = ">";
+            this.btnAdd.Text = "Thêm";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(28, 31);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.Size = new System.Drawing.Size(39, 20);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Khoa";
+            this.label2.Text = "Bàn";
             // 
-            // cbbKhoa
+            // cbbBan
             // 
-            this.cbbKhoa.FormattingEnabled = true;
-            this.cbbKhoa.Items.AddRange(new object[] {
-            "_____________________",
-            "Công Nghệ Thông Tin",
-            "Quản Trị Khách Sạn",
-            "Quản Trị Kinh Doanh",
-            "Công Nghệ Ô TÔ",
-            "Chuyên Ngữ Anh"});
-            this.cbbKhoa.Location = new System.Drawing.Point(21, 54);
-            this.cbbKhoa.Name = "cbbKhoa";
-            this.cbbKhoa.Size = new System.Drawing.Size(198, 28);
-            this.cbbKhoa.TabIndex = 0;
+            this.cbbBan.FormattingEnabled = true;
+            this.cbbBan.Items.AddRange(new object[] {
+            "Chọn bàn"});
+            this.cbbBan.Location = new System.Drawing.Point(21, 54);
+            this.cbbBan.Name = "cbbBan";
+            this.cbbBan.Size = new System.Drawing.Size(198, 28);
+            this.cbbBan.TabIndex = 0;
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Controls.Add(this.lvMon);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.Location = new System.Drawing.Point(58, 188);
             this.groupBox2.Name = "groupBox2";
@@ -178,6 +216,43 @@
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Danh sách món";
+            // 
+            // lvMon
+            // 
+            this.lvMon.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.chMon,
+            this.columnHeader3});
+            this.lvMon.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvMon.FullRowSelect = true;
+            this.lvMon.GridLines = true;
+            this.lvMon.HideSelection = false;
+            listViewItem1.Tag = "0";
+            this.lvMon.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3});
+            this.lvMon.Location = new System.Drawing.Point(3, 22);
+            this.lvMon.Name = "lvMon";
+            this.lvMon.Size = new System.Drawing.Size(357, 335);
+            this.lvMon.TabIndex = 0;
+            this.lvMon.UseCompatibleStateImageBehavior = false;
+            this.lvMon.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Tag = "1";
+            this.columnHeader1.Text = "STT";
+            // 
+            // chMon
+            // 
+            this.chMon.Text = "Tên Món";
+            this.chMon.Width = 148;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Giá Món";
+            this.columnHeader3.Width = 79;
             // 
             // label1
             // 
@@ -190,38 +265,6 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "QUẢN LÝ ĐẶT MÓN";
             // 
-            // listView1
-            // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2,
-            this.columnHeader3});
-            this.listView1.HideSelection = false;
-            listViewItem1.Tag = "0";
-            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1});
-            this.listView1.Location = new System.Drawing.Point(7, 26);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(356, 328);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Tag = "1";
-            this.columnHeader1.Text = "STT";
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "Tên Món";
-            this.columnHeader2.Width = 148;
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "Giá Món";
-            this.columnHeader3.Width = 79;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -233,6 +276,7 @@
             this.Controls.Add(this.groupBox2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox4.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
@@ -245,22 +289,24 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.ListView listViewSV;
-        private System.Windows.Forms.ColumnHeader lvHoTen;
-        private System.Windows.Forms.ColumnHeader lvKhoa;
+        private System.Windows.Forms.ListView lvDatMon;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.Button btnRemoveAll;
         private System.Windows.Forms.Button btnRemove;
-        private System.Windows.Forms.Button btnAddAll;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox cbbKhoa;
+        private System.Windows.Forms.ComboBox cbbBan;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView lvMon;
         private System.Windows.Forms.ColumnHeader columnHeader1;
-        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader chMon;
         private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox CbSoLuong;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
     }
 }
 
--- B4_02/Form1.cs
+++ B4_02/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using ListView = System.Windows.Forms.ListView;
 
 namespace B4_02
 {
@@ -16,5 +18,83 @@ namespace B4_02
         {
             InitializeComponent();
         }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+                cbbBan.Items.Add("bàn 1");
+                cbbBan.Items.Add("bàn 2");
+                cbbBan.Items.Add("bàn 3");
+                cbbBan.Items.Add("bàn 4");
+                cbbBan.Items.Add("bàn 5");
+                cbbBan.Items.Add("bàn 6");
+                cbbBan.Items.Add("bàn 7");
+                cbbBan.Items.Add("bàn 8");
+                cbbBan.Items.Add("bàn 9");
+                cbbBan.Items.Add("bàn 10");
+                //lbSinhVien.Items.Add("Trần Quốc Huy(Nghiện)");
+                //lbSinhVien.Items.Add("Vũ Minh hiếu");
+                //lbSinhVien.Items.Add("Trần Minh Phát");
+                cbbBan.SelectedIndex = 0;
+                CbSoLuong.SelectedIndex = 0;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ChonBan = cbbBan.SelectedIndex.ToString();
+                ListView monlv = lvMon as ListView;
+                ListViewItem item = monlv.SelectedItems[0];
+                if (ChonBan == "0")
+                {
+                    throw new Exception("Vui lòng chọn bàn");
+                }
+                if (item.Checked)
+                {
+                    throw new Exception("Vui lòng chọn món");
+                }
+                else
+                {
+                    string ban = cbbBan.SelectedItem.ToString();
+                    string soluong = CbSoLuong.SelectedItem.ToString();
+                    string tenmon = " ";
+                    string gia = " ";
+                    ListViewItem item1 = new ListViewItem(ban);
+                    if (item.Text == "01")
+                     {
+                      tenmon = "Cà phê đá";
+                       gia = "15000 ";
+                    }
+                    else
+                    {
+
+                        if(item.Text == "02")
+                        {
+                            tenmon = "Cà phê sữa";
+                            gia = "18000 ";
+
+                        }
+                        else
+                        {
+                            tenmon = "Nước cam";
+                            gia = "25000 ";
+
+                        }
+                    }
+                        item1.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = tenmon });
+                        item1.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = soluong });
+                        item1.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = gia });
+                        lvDatMon.Items.Add(item1);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }        
     }
 }
