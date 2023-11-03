9b528740d9d1f2ad2c5dc1591fbbd4157b051982
VuHieu257 <144141957+VuHieu257@users.noreply.github.com>
2023-11-03 18:37:50 +0700

    Add files via upload
    Buoi3

---
+++ Buoi3/B03_btvn/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace B03_btvn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtDanhSach = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnOut = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.mtbMaS = new System.Windows.Forms.MaskedTextBox();
+            this.mtbTen = new System.Windows.Forms.MaskedTextBox();
+            this.mtbGia = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.mtbSL = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtDanhSach);
+            this.groupBox3.Location = new System.Drawing.Point(471, 221);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(433, 305);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Danh sách";
+            // 
+            // txtDanhSach
+            // 
+            this.txtDanhSach.Location = new System.Drawing.Point(26, 33);
+            this.txtDanhSach.Multiline = true;
+            this.txtDanhSach.Name = "txtDanhSach";
+            this.txtDanhSach.ReadOnly = true;
+            this.txtDanhSach.Size = new System.Drawing.Size(375, 245);
+            this.txtDanhSach.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnOut);
+            this.groupBox2.Controls.Add(this.btnDelete);
+            this.groupBox2.Controls.Add(this.btnAdd);
+            this.groupBox2.Location = new System.Drawing.Point(471, 140);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(433, 65);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chức năng";
+            // 
+            // btnOut
+            // 
+            this.btnOut.Location = new System.Drawing.Point(239, 22);
+            this.btnOut.Name = "btnOut";
+            this.btnOut.Size = new System.Drawing.Size(90, 37);
+            this.btnOut.TabIndex = 0;
+            this.btnOut.Text = "Thoát";
+            this.btnOut.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(121, 22);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(90, 37);
+            this.btnDelete.TabIndex = 0;
+            this.btnDelete.Text = "Xóa DS";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(16, 22);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(90, 37);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Thêm DS";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(370, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "THÔNG TIN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mã Sách:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tên Sách";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Giá:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 215);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 16);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Số Lượng";
+            // 
+            // mtbMaS
+            // 
+            this.mtbMaS.Location = new System.Drawing.Point(102, 46);
+            this.mtbMaS.Mask = "00000000";
+            this.mtbMaS.Name = "mtbMaS";
+            this.mtbMaS.Size = new System.Drawing.Size(148, 22);
+            this.mtbMaS.TabIndex = 6;
+            // 
+            // mtbTen
+            // 
+            this.mtbTen.Location = new System.Drawing.Point(102, 98);
+            this.mtbTen.Name = "mtbTen";
+            this.mtbTen.Size = new System.Drawing.Size(148, 22);
+            this.mtbTen.TabIndex = 6;
+            // 
+            // mtbGia
+            // 
+            this.mtbGia.Location = new System.Drawing.Point(102, 153);
+            this.mtbGia.Name = "mtbGia";
+            this.mtbGia.Size = new System.Drawing.Size(148, 22);
+            this.mtbGia.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.mtbSL);
+            this.groupBox1.Controls.Add(this.mtbGia);
+            this.groupBox1.Controls.Add(this.mtbTen);
+            this.groupBox1.Controls.Add(this.mtbMaS);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(100, 140);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 263);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhập thông tin";
+            // 
+            // mtbSL
+            // 
+            this.mtbSL.Location = new System.Drawing.Point(102, 209);
+            this.mtbSL.Name = "mtbSL";
+            this.mtbSL.Size = new System.Drawing.Size(148, 22);
+            this.mtbSL.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 588);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtDanhSach;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnOut;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox mtbMaS;
+        private System.Windows.Forms.MaskedTextBox mtbTen;
+        private System.Windows.Forms.MaskedTextBox mtbGia;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MaskedTextBox mtbSL;
+    }
+}
+
---
+++ Buoi3/B03_btvn/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B03_btvn
+{
+    public partial class Form1 : Form
+    {
+        private int stt;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (mtbMaS.Text.Length == 0 || mtbTen.Text.Length == 0 || mtbGia.Text.Length== 0||mtbSL.Text.Length!=0)
+                {
+                    MessageBox.Show("Không được để trống các ô");
+                }
+                string MaS = mtbMaS.Text;
+                string  Ten = mtbTen.Text;
+                string Gia = (mtbGia.Text);
+                string Sl= (mtbSL.Text);
+                txtDanhSach.Text += stt + " - Mã Sách:" + MaS + "- Tên Sách:" + Ten + "- Giá Sách:" + Gia + "- Số lượng" + Sl +"\r\n";
+                stt++;
+                mtbMaS.Text = "";
+                mtbTen.Text = "";
+                mtbGia.Text = "";
+                mtbSL.Text = "";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnOut_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            txtDanhSach.Text = "";
+        }
+
+      
+    }
+}
