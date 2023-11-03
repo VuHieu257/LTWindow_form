397fc217bec6630e11f388333ff411959447aca1
VuHieu257 <144141957+VuHieu257@users.noreply.github.com>
2023-11-03 18:17:55 +0700

    Add files via upload
    bài tập Buổi 1

---
+++ Buoi2/Buoi2_02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi2_02
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            SinhVien sv = new SinhVien();
+            List<SinhVien> listSinhVien = sv.NhapSinhVien();
+            Console.WriteLine("-----------------------------");
+            sv.xuatSinhVien(listSinhVien);
+            Console.WriteLine("-----------------------------");
+            sv.DSSVCNTT(listSinhVien);
+            Console.WriteLine("-----------------------------");
+            sv.DSSVDTB(listSinhVien);
+            Console.WriteLine("-----------------------------");
+            sv.DSSVDSapXepTB(listSinhVien);
+            Console.WriteLine("-----------------------------");
+            sv.DSSVD1_5(listSinhVien);
+            Console.WriteLine("-----------------------------");
+            sv.DSSVD1_6(listSinhVien);
+            Console.ReadKey();            
+        }
+
+    }
+}
---
+++ Buoi2/Buoi2_bai03/Teacher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi2_bai03
+{
+    internal class Teacher : person
+    {
+        public string MaGv { get; set; }
+        public string Khoa { get; set; }
+        public string ChucVu { get; set; }
+        public Teacher() { }
+        public Teacher(string magv,string khoa,string chuvu,string cmnd, string hoten, string diaChi) : base(cmnd, hoten, diaChi)
+        {
+            MaGv = magv;
+            Khoa = khoa;
+            ChucVu = chuvu;
+        }
+        public void NhapSD()
+        {
+            Console.Write("Nhap ma GV:");
+            MaGv = Console.ReadLine();
+            Console.Write("Nhap Chuc Vu:");
+            ChucVu = Console.ReadLine();
+            Console.Write("Nhap Khoa:");
+            Khoa = Console.ReadLine();
+
+        }
+        public void XuatSD()
+        {
+            Console.WriteLine("MaSV:{0}\tDiemTB:{1}\tKhoa:{2}", MaGv, ChucVu, Khoa);
+        }
+    }
+}
